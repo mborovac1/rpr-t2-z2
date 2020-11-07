c430a2e4d84c37215109de0dd1eda7abd5e26583
mborovac1 <mborovac1@etf.unsa.ba>
2020-11-07 19:19:27 +0100

    Implementirani konstruktori u klasi Interval

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -4,5 +4,21 @@ public class Interval {
     private double pocetnaTacka, krajnjaTacka;
     private boolean pripadaPocetna, pripadaKrajnja;
 
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pripadaPocetna, boolean pripadaKrajnja) {
+        if(pocetnaTacka > krajnjaTacka)
+            throw new IllegalArgumentException("");
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pripadaPocetna = pripadaPocetna;
+        this.pripadaKrajnja = pripadaKrajnja;
+    }
+
+    public Interval() {
+        pocetnaTacka = 0;
+        krajnjaTacka = 0;
+        pripadaPocetna = false;
+        pripadaKrajnja = false;
+    }
+
 
 }
